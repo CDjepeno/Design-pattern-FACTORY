6340c1c1ff0e047632379e0af71d995bfa5ec9e7
CDjepeno <djepeno.christopher@gmail.com>
2021-07-22 23:10:36 +0200

    delete public folder

--- public/classes/CheesePizzaParis.js
+++
@@ -1,7 +0,0 @@
-import { Pizza } from '../abstract/Pizza.js';
-export class CheesePizzaParis extends Pizza {
-    constructor() {
-        super("Pizza sauce style paris et fromage", "Pâte fine", "Sauce Marinara");
-        this.garnitures.push("Paramigiano reggiano râpé");
-    }
-}
